02f65331f3181bae7bb0f2a486a0ea9b13147fd8
Jacob Sheehy <jacob.sheehy@gmail.com>
2013-05-11 10:08:07 -0700

    Adding API_KEY field to config

--- src/ca/cumulonimbus/barometernetwork/PressureNETConfiguration.java
+++ src/ca/cumulonimbus/barometernetwork/PressureNETConfiguration.java
@@ -5,5 +5,5 @@ public final class PressureNETConfiguration {
 	public static final String SERVER_URL = "";
 	public static final String PRESSURENET_URL = "";
 	public static final String HTTPS_PASSWORD = "";
-	
+	public static final String API_KEY = "";
 }
