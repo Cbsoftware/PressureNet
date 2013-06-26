fef94ec15867f8f58a60a149b16f0a6e6cfd4596
Jacob Sheehy <jacob.sheehy@gmail.com>
2013-06-26 11:47:48 -0400

    Clean location editing, preparing for db

--- src/ca/cumulonimbus/barometernetwork/EditLocationActivity.java
+++ src/ca/cumulonimbus/barometernetwork/EditLocationActivity.java
@@ -1,74 +1,36 @@
 package ca.cumulonimbus.barometernetwork;
 
-import ca.cumulonimbus.pressurenetsdk.CbApiCall;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.GoogleMap.OnCameraChangeListener;
-import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.Marker;
-
 import android.app.Activity;
-import android.content.Context;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class EditLocationActivity extends Activity {
 
-	private GoogleMap mMap;
+	private EditText editLocationName;
+	private EditText editLatitude;
+	private EditText editLongitude;
+	private Button buttonSave;
+	private Button buttonCancel;
+	private Button buttonDelete;
+	
+	public void populateFields() {
+		editLocationName = (EditText) findViewById(R.id.editLocationName);
+		editLatitude = (EditText) findViewById(R.id.editLocationLatitude);
+		editLongitude = (EditText) findViewById(R.id.editLocationLongitude);
+
+		buttonCancel = (Button) findViewById(R.id.buttonCancelLocation);
+		buttonSave = (Button) findViewById(R.id.buttonSaveLocation);
+		
+	}
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.edit_location);
-		setUpMap();
-	}
-
-
-	private void setUpMapIfNeeded() {
-		// Do a null check to confirm that we have not already instantiated the
-		// map.
-		if (mMap == null) {
-			mMap = ((MapFragment) getFragmentManager().findFragmentById(
-					R.id.mapLocation)).getMap();			
 		
-			// Check if we were successful in obtaining the map.
-			if (mMap != null) {
-				// The Map is verified. It is now safe to manipulate the map.
-			}
-		}
-
-	}
-
-	// Zoom into the user's location, add pinch zoom controls
-	public void setUpMap() {
-		setUpMapIfNeeded();
-
-		mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.mapLocation))
-				.getMap();
-
-		// TODO: Set default coordinates (centered at the location)
-
-	}
-
-	public void moveMapTo(double latitude, double longitude) {
-		setUpMapIfNeeded();
-
-		mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.mapLocation))
-				.getMap();
-		try {
-			mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(
-					latitude, longitude), 13));
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		populateFields();
 	}
 	
+
 }
