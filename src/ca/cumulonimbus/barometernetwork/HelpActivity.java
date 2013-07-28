5435e0719d05cf1fe59b88dc0d3947481c6f2601
Jacob Sheehy <jacob.sheehy@gmail.com>
2013-07-28 17:53:20 -0400

    ActionBar updates

--- src/ca/cumulonimbus/barometernetwork/HelpActivity.java
+++ src/ca/cumulonimbus/barometernetwork/HelpActivity.java
@@ -1,7 +1,10 @@
 package ca.cumulonimbus.barometernetwork;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HelpActivity extends Activity {
 
@@ -9,5 +12,11 @@ public class HelpActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.help);
+		
+		ActionBar bar = getActionBar();
+		int actionBarTitleId = getResources().getSystem().getIdentifier("action_bar_title", "id", "android");
+		
+		TextView actionBarTextView = (TextView)findViewById(actionBarTitleId); 
+		actionBarTextView.setTextColor(Color.WHITE);
 	}
 }
--- src/ca/cumulonimbus/barometernetwork/PreferencesActivity.java
+++ src/ca/cumulonimbus/barometernetwork/PreferencesActivity.java
@@ -1,7 +1,10 @@
 package ca.cumulonimbus.barometernetwork;
 
+import android.app.ActionBar;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.widget.TextView;
 
 public class PreferencesActivity extends PreferenceActivity{
 
@@ -9,5 +12,11 @@ public class PreferencesActivity extends PreferenceActivity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
+		
+		ActionBar bar = getActionBar();
+		int actionBarTitleId = getResources().getSystem().getIdentifier("action_bar_title", "id", "android");
+		
+		TextView actionBarTextView = (TextView)findViewById(actionBarTitleId); 
+		actionBarTextView.setTextColor(Color.WHITE);
 	}
 }
