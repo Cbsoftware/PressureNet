fa267ad05a15609ed65880c6d925fc2c50b63857
Jacob Sheehy <jacob.sheehy@gmail.com>
2013-11-22 11:19:56 -0500

    Adding preference for condition notifications, connecting to What's New, #78

--- src/ca/cumulonimbus/barometernetwork/WhatsNewActivity.java
+++ src/ca/cumulonimbus/barometernetwork/WhatsNewActivity.java
@@ -1,17 +1,23 @@
 package ca.cumulonimbus.barometernetwork;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.TextView;
 
 public class WhatsNewActivity extends Activity {
 
 	TextView pressureNETVersion;
 	Button done;
+	CheckBox enableConditionNotifications;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,12 @@ public class WhatsNewActivity extends Activity {
 		String versionName = "";
 		pressureNETVersion = (TextView) findViewById(R.id.textWhatsNewTitle);
 		done = (Button) findViewById(R.id.buttonDone);
+		enableConditionNotifications = (CheckBox) findViewById(R.id.checkReceiveConditionNotifications);
+		
+		SharedPreferences prefs  = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+		boolean checkedNotifs = prefs.getBoolean("send_condition_notifications", false);
+		enableConditionNotifications.setChecked(checkedNotifs);
+		
 		try {
 			versionName = this.getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName;
 		} catch(NameNotFoundException nnfe) {
@@ -32,6 +44,18 @@ public class WhatsNewActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				finish();
+			}
+		});
+		
+		enableConditionNotifications.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+				SharedPreferences.Editor editor = sharedPreferences.edit();
+				editor.putBoolean("send_condition_notifications", isChecked);
+				editor.commit();
+			
 			}
 		});
 	}
