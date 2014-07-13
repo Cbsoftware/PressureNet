7f3d5c75a53a8b7e77bd0908f85b14964890f8d3
Jacob Sheehy <jacob.sheehy@gmail.com>
2014-07-13 15:42:06 -0400

    Adding new notifications prompt, #200

--- src/ca/cumulonimbus/barometernetwork/NewWelcomeNotificationsFragment.java
+++ src/ca/cumulonimbus/barometernetwork/NewWelcomeNotificationsFragment.java
@@ -6,20 +6,44 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class NewWelcomeNotificationsFragment extends Fragment {
+public class NewWelcomeNotificationsFragment extends Fragment implements OnClickListener {
 
 	Context context;
+	
+	Button noThanks;
+	Button yesPlease;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.new_welcome_notifications, null);
 		context = v.getContext();
-		// summary = (TextView) v.findViewById(R.id.home_summary);
+		
+		noThanks = (Button) v.findViewById(R.id.newWelcomeNotificationsNoButton);
+		yesPlease = (Button) v.findViewById(R.id.newWelcomeNotificationsYesButton);
 
-		// updateView();
+		noThanks.setOnClickListener(this);
+		yesPlease.setOnClickListener(this);
+		
 		return v;
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()) {
+		case R.id.newWelcomeNotificationsNoButton:
+			
+			getActivity().finish();
+			break;
+		case R.id.newWelcomeNotificationsYesButton:
+			
+			getActivity().finish();
+			break;
+			
+		default:
+		}
 	}
 }
