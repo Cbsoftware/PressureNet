7bdae987eb86deb12a20dc90fc2d734a16083769
Jacob Sheehy <jacob.sheehy@gmail.com>
2015-01-02 18:55:28 -0500

    Preparing alarmpad integration, #221

--- src/ca/cumulonimbus/extensions/alarmpad/AlarmPadExtension.java
+++ src/ca/cumulonimbus/extensions/alarmpad/AlarmPadExtension.java
@@ -6,6 +6,9 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
@@ -28,6 +31,8 @@ public class AlarmPadExtension extends MindMeExtension {
 	private Messenger mMessenger = new Messenger(new IncomingHandler());
 	Messenger mService = null;
 
+	private CbCurrentCondition alarmCondition = null;
+	
 	/**
 	 * Communicate with CbService
 	 */
@@ -37,7 +42,10 @@ public class AlarmPadExtension extends MindMeExtension {
 			mService = new Messenger(service);
 			mBound = true;
 			Message msg = Message.obtain(null, CbService.MSG_OKAY);
-
+			
+			CbApiCall conditionApi = buildMapCurrentConditionsCall(2);
+			makeCurrentConditionsAPICall(conditionApi);
+			
 		}
 
 		public void onServiceDisconnected(ComponentName className) {
@@ -53,6 +61,16 @@ public class AlarmPadExtension extends MindMeExtension {
 		bindService(new Intent(getApplicationContext(), CbService.class),
 				mConnection, Context.BIND_AUTO_CREATE);
 	}
+	
+	
+
+	@Override
+	public void onDestroy() {
+		unbindService(mConnection);
+		super.onDestroy();
+	}
+
+
 
 	@Override
 	protected void onUpdateData(int reason) {
@@ -63,7 +81,7 @@ public class AlarmPadExtension extends MindMeExtension {
 			// Manual updates are called every time AlarmPad needs to update its PressureNet cache.
 			// In this case the extension should call publishUpdate() to let AlarmPad know about
 			// new data available before showing it on an alarm screen.
-
+			
 			updateAlarmPadCache();
 			
 			//We do not stop the service here because we need to wait for the results of API calls
@@ -74,6 +92,7 @@ public class AlarmPadExtension extends MindMeExtension {
 			// an alarm rings
 
 			// TODO
+			
 			
 			stopSelf();
 		}
@@ -89,7 +108,83 @@ public class AlarmPadExtension extends MindMeExtension {
 //		rv.setTextViewText(R.id.textViewTitle, horoscopeTitle);
 //		rv.setTextViewText(R.id.textViewDescription, horoscopeDescription);
 
-		askForCurrentConditionRecents();
+		
+		if(!mBound) {
+			Log.v(TAG, "updating alarmpadcache, not bound so binding");
+			
+			bindService(new Intent(getApplicationContext(), CbService.class),
+					mConnection, Context.BIND_AUTO_CREATE);
+		} else {
+			
+			// askForCurrentConditionRecents();
+			// Sets up the data to publish
+			CbApiCall conditionApi = buildMapCurrentConditionsCall(2);
+			makeCurrentConditionsAPICall(conditionApi);
+			
+			if(alarmCondition!=null) {
+				Log.v(TAG, "update cache with condition " + alarmCondition.getGeneral_condition());
+				ExtensionData data = new ExtensionData()
+						.visible(true)
+						.icon(R.drawable.ic_notification)
+						.statusToDisplay(getString(R.string.extension_description));
+	
+				// Publish the extension data update
+				publishUpdate(data);
+				stopSelf();
+			} else {
+				Log.v(TAG, "condition is null");
+			}
+		}
+		
+	
+
+	}
+	
+	private CbApiCall buildMapCurrentConditionsCall(double hoursAgo) {
+		Log.v(TAG, "alarmpad building map conditions call for hours: " + hoursAgo);
+		long startTime = System.currentTimeMillis()
+				- (int) ((hoursAgo * 60 * 60 * 1000));
+		long endTime = System.currentTimeMillis();
+		CbApiCall api = new CbApiCall();
+
+		LocationManager lm = (LocationManager) this
+				.getSystemService(Context.LOCATION_SERVICE);
+		Location loc = lm
+				.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+		
+		
+		double minLat = loc.getLatitude() - 0.1;
+		double maxLat = loc.getLatitude() + 0.1;
+		double minLon = loc.getLongitude() - 0.1;
+		double maxLon = loc.getLongitude() + 0.1;
+
+		api.setMinLat(minLat);
+		api.setMaxLat(maxLat);
+		api.setMinLon(minLon);
+		api.setMaxLon(maxLon);
+		api.setStartTime(startTime);
+		api.setEndTime(endTime);
+		api.setLimit(500);
+		api.setCallType("Conditions");
+		return api;
+	}
+	
+	private void makeCurrentConditionsAPICall(CbApiCall apiCall) {
+		Log.v(TAG, "alarmpad making current conditions api call");
+		if (mBound) {
+			Message msg = Message.obtain(null,
+					CbService.MSG_MAKE_CURRENT_CONDITIONS_API_CALL, apiCall);
+			try {
+				msg.replyTo = mMessenger;
+				mService.send(msg);
+				
+			} catch (RemoteException e) {
+				// e.printStackTrace();
+			}
+		} else {
+			System.out
+					.println("alarmpad data management error: not bound for condition api");
+		}
 	}
 	
 	/**
@@ -100,8 +195,8 @@ public class AlarmPadExtension extends MindMeExtension {
 	private void askForCurrentConditionRecents() {
 
 		if (mBound) {
-			Log.v(TAG, "asking for current conditions");
-			CbApiCall api = new CbApiCall();
+			Log.v(TAG, "alarmpad asking for current conditions");
+			CbApiCall api = buildMapCurrentConditionsCall(2);
 			Message msg = Message.obtain(null,
 					CbService.MSG_GET_CURRENT_CONDITIONS, api);
 			try {
@@ -129,16 +224,24 @@ public class AlarmPadExtension extends MindMeExtension {
 				ArrayList<CbCurrentCondition> receivedList = (ArrayList<CbCurrentCondition>) msg.obj;
 				if (receivedList != null) {
 					if (receivedList.size() > 0) {
-						Log.i(TAG, "conditions received");
+						Log.i(TAG, "alarmpad conditions received");
 						for (CbCurrentCondition c : receivedList) {
-							
+							alarmCondition = c;
+							Log.v(TAG, "app received " + c.getGeneral_condition());
 						}
 						
+						/*
 						// Sets up the data to publish
 						ExtensionData data = new ExtensionData()
 								.visible(true)
 								.icon(R.drawable.ic_notification)
 								.statusToDisplay(getString(R.string.extension_description));
+						*/
+						// Sets up the data to publish
+						ExtensionData data = new ExtensionData()
+								.visible(true)
+								.icon(R.drawable.ic_launcher)
+								.statusToDisplay(receivedList.get(0).getGeneral_condition());
 
 						// Publish the extension data update
 						publishUpdate(data);
@@ -152,6 +255,11 @@ public class AlarmPadExtension extends MindMeExtension {
 					break;
 				}
 
+				break;
+			case CbService.MSG_API_RESULT_COUNT:
+				int count = msg.arg1;
+				Log.v(TAG, "api res count, gonna ask for recents , count " + count);
+				askForCurrentConditionRecents();
 				break;
 			default:
 				Log.v(TAG, "received default message");
